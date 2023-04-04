dfb56cc127ce802ffa6c48b515acfb05f1da6183
Darlison <moisesdarlison91@gmail.com>
2023-04-04 18:36:39 -0300

    test api

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ import express from "express";
 const app = express();
 app.use(express.json())
 app.get('/', (req, res) => {
-    console.log("Init request hehe");
+    console.log("Init request ");
     return res.status(200).json({ Message: "Success!" })
 })
 const { PORT = 3333 } = process.env
-app.listen(PORT, () => console.log("Listen on port:", PORT, "\n", new Date()))
+app.listen(PORT, () => console.log("Update: 01 - Listen on port:", PORT, "\n", new Date()))
