022ad8bc34516b8eb1246063d28165b493cf51f0
Darlison <moisesdarlison91@gmail.com>
2023-04-04 17:22:56 -0300

    fix: new test

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.get('/', (req, res) => {
     res.status(200).send(`Hello TESTE!`);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`Temos uma Att...`);
+    console.log(`Temos uma Att 3...`);
     console.log(`Server listening on port ${port}`);
 });
